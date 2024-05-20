820a9deacebb604373134bb5d82626a3e24221b0
Leo Sundström <leo.sundstrom@elev.ga.ntig.se>
2024-05-20 10:29:36 +0200

    NetworkREST

---
+++ henry/Planet.cs
@@ -0,0 +1,16 @@
+﻿namespace henry;
+
+public class Planet
+{
+    public string name {get; set;}
+
+    public string population {get; set;}
+    
+    public string terrain {get; set;}
+
+    public string rotation_period {get; set;}
+    
+    public string orbital_period {get; set;}
+    
+
+}
---
+++ henry/Program.cs
@@ -0,0 +1,29 @@
+﻿using henry;
+using RestSharp;
+using System.Text.Json;
+
+RestClient client = new("https://swapi.py4e.com/api/");
+
+
+string search = "2";
+Console.WriteLine("Enter a number");
+
+search = Console.ReadLine();
+
+Console.Clear();
+
+RestRequest request = new($"planets/{search}/");
+
+RestResponse response = client.GetAsync(request).Result;
+
+File.WriteAllText("pokemon.json", response.Content);
+
+Planet planet = JsonSerializer.Deserialize<Planet>(response.Content);
+
+Console.WriteLine("Name: " + planet.name);
+Console.WriteLine("Population: " + planet.population);
+Console.WriteLine("Terrain: " + planet.terrain);
+Console.WriteLine("Rotation Period: " + planet.rotation_period);
+Console.WriteLine("Orbital Period: " + planet.orbital_period);
+
+Console.ReadLine();
